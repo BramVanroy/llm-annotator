65b524f96191f56133d06d0b1657bb471a0e01cb
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2025-12-18 14:44:30 +0100

    add stub fitler

--- examples/wiki-nl-mcq/preprocess_finewiki_nl.py
+++ examples/wiki-nl-mcq/preprocess_finewiki_nl.py
@@ -3,6 +3,12 @@ from datasets import load_dataset
 from llm_annotator.utils import get_hf_username
 
 
+def is_stub(wikitext: str):
+    # Thanks to Edwin Rijgersberg for spotting this pattern of stubs!
+    # E.g. overview pages like https://nl.wikipedia.org/wiki/Categorie:Wikipedia:Beginnetje_biologie
+    return r"{{beginnetje" in wikitext.lower()
+
+
 def main(num_workers: int | None = None):
     hf_user = get_hf_username()
 
@@ -12,10 +18,14 @@ def main(num_workers: int | None = None):
     # Must contain between 30 and 24,000 words
     # (Add space at the end to count the last sentence if it ends with a period)
     ds = ds.filter(
-        lambda text: f"{text} ".count(". ") >= 3 and 30 < len(text.split()) < 24_000 and 200 < len(text) < 72_000,
-        input_columns="text",
+        lambda text, wikitext: f"{text} ".count(". ") >= 3
+        and 30 < len(text.split()) < 24_000
+        and 200 < len(text) < 72_000
+        and not is_stub(wikitext),
+        input_columns=["text", "wikitext"],
         num_proc=num_workers,
     )
+    print(ds)
 
     ds.push_to_hub(f"{hf_user}/finewiki-nl-30-to-24k-tokens")
 
